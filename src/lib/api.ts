664ef5246b472895f5d13e685467a1cf5b9651a9
Miguel Perez <miguel.per@edu.312school.com>
2025-05-30 20:40:29 -0400

    Created api file under src/frontend repo for backend url

---
+++ src/lib/api.ts
@@ -0,0 +1,19 @@
+const isDev = import.meta.env.DEV;
+
+export const API_URL = import.meta.env.VITE_API_URL;
+
+export const fetchBudgets = async () => {
+  const res = await fetch(API_URL);
+  return await res.json();
+};
+
+export const createBudget = async (title: string, amount: number) => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, amount }),
+  });
+  return await res.json();
+};
